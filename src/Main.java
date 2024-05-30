32109788c978699e7853e8c423510783cea89e88
TADIO1829 <151254512+TADIO1829@users.noreply.github.com>
2024-05-29 22:10:56 -0500

    CreacionDeClases settersyGetters

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,37 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Hoteles hotel1 = new Hoteles();
+        hotel1.setNombre("Hotel Paradise");
+        hotel1.setFundador("Jqwe");
+        hotel1.setCategoria("5 estrellas");
+        hotel1.setAnio(2010);
+        hotel1.setHabitaciones(100);
+        hotel1.setPisos(4);
 
+        Hoteles hotel2 = new Hoteles();
+        hotel2.setNombre("asd");
+        hotel2.setFundador("qwer");
+        hotel2.setCategoria("5 estrellas");
+        hotel2.setAnio(2022);
+        hotel2.setHabitaciones(300);
+        hotel2.setPisos(6);
+
+        Hoteles hotel3 = new Hoteles();
+        hotel3.setNombre("qweo");
+        hotel3.setFundador("Belen Martinez");
+        hotel3.setCategoria("xzcxzc");
+        hotel3.setAnio(2023);
+        hotel3.setHabitaciones(100);
+        hotel3.setPisos(3);
+
+        Hoteles hotel4 = new Hoteles();
+        hotel4.setNombre("qwerqw");
+        hotel4.setFundador("xzcwqez ");
+        hotel4.setCategoria("4 estrellas");
+        hotel4.setAnio(2019);
+        hotel4.setHabitaciones(600);
+        hotel4.setPisos(6);
     }
-    //HOLA Alison
+    //HOLA Alison+
 }
