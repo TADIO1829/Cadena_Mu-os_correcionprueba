cbef5a3935d688e51e19254f5ee055dd17fcc0b5
TADIO1829 <151254512+TADIO1829@users.noreply.github.com>
2024-05-29 20:52:54 -0500

    CreacionDeClases settersyGetters

--- src/Hoteles.java
+++ src/Hoteles.java
@@ -17,4 +17,52 @@ public class Hoteles {
         this.categoria = categoria;
         this.fundador = fundador;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPisos() {
+        return pisos;
+    }
+
+    public void setPisos(int pisos) {
+        this.pisos = pisos;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public int getHabitaciones() {
+        return habitaciones;
+    }
+
+    public void setHabitaciones(int habitaciones) {
+        this.habitaciones = habitaciones;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getFundador() {
+        return fundador;
+    }
+
+    public void setFundador(String fundador) {
+        this.fundador = fundador;
+    }
 }
